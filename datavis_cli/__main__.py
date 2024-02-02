3b1e90dfa5636cd72bd10aedd3f90b91bc6e22dc
Ben Skubi <bskubi@gmail.com>
2024-02-01 20:12:14 -0800

    Changed to datavis-cli and reduced scope to just output boilerplate code to stdin

--- datavis_cli/__main__.py
+++ datavis_cli/__main__.py
@@ -1,45 +1,37 @@
-# notebook_vis_cli.py
 import click
-import nbformat
 import os
 import yaml
 import pandas as pd
 
-# Function to create a new Jupyter notebook or load an existing one
-def load_or_create_notebook(filename):
-    if os.path.exists(filename):
-        with open(filename) as f:
-            return nbformat.read(f, as_version=4)
-    else:
-        return nbformat.v4.new_notebook()
-
-# Function to add a cell to a notebook
-def add_cell(notebook, cell_type, content):
-    cell = nbformat.v4.new_code_cell(content) if cell_type == 'code' else nbformat.v4.new_markdown_cell(content)
-    notebook.cells.append(cell)
-
-# Function to save the notebook
-def save_notebook(notebook, filename):
-    with open(filename, 'w') as f:
-        nbformat.write(notebook, f)
+@click.group()
+def cli():
+    """
+    CLI tool to output boilerplate code for data loading and visualization
+    """
+    pass
 
 # Function to create the data loading cell
-def create_data_loading_cell(filename):
-    return f'''
+@click.command()
+@click.argument('filename')
+@click.argument('dataframe_name')
+def load_dataframe(filename, dataframe_name):
+    click.echo(f'''
 import pandas as pd
 import yaml
 
 with open("{filename}", 'r') as file:
-    data = yaml.safe_load(file)
+    {dataframe_name} = pd.DataFrame(yaml.safe_load(file))
+print({dataframe_name}.head())
+print({dataframe_name}.describe())
+''')
 
-df = pd.DataFrame(data)
-print(df.head())
-print(df.describe())
-'''
-
-# Function to create a seaborn heatmap cell
-def create_heatmap_cell():
-    return '''
+@click.command()
+@click.argument('dataframe_name')
+def heatmap(dataframe_name):
+    """
+    Output code to generate Seaborn heatmap
+    """
+    click.echo(f'''
 import seaborn as sns
 import matplotlib.pyplot as plt
 import colorcet as cc
@@ -50,7 +42,7 @@ import colorcet as cc
 # For detailed information on the parameters, visit:
 # https://seaborn.pydata.org/generated/seaborn.heatmap.html#seaborn.heatmap
 
-sns.heatmap(df, 
+sns.heatmap({dataframe_name}, 
             vmin=None, 
             vmax=None, 
             cmap=cc.cm.CET_CBL1, 
@@ -69,13 +61,16 @@ sns.heatmap(df,
             yticklabels='auto', 
             mask=None, 
             ax=None)
+''')
 
-plt.show()
-'''
 
-# Function to create a seaborn clustermap cell
-def create_clustermap_cell():
-    return '''
+@click.command()
+@click.argument('dataframe_name')
+def clustermap(dataframe_name):
+    """
+    Output code to generate Seaborn clustermap
+    """
+    click.echo(f'''
 import seaborn as sns
 import matplotlib.pyplot as plt
 import colorcet as cc
@@ -84,7 +79,7 @@ import colorcet as cc
 # See https://colorcet.com/index.html for details
 # Seaborn clustermap documentation
 # https://seaborn.pydata.org/generated/seaborn.clustermap.html
-sns.clustermap(df, 
+sns.clustermap({dataframe_name}, 
                pivot_kws=None, 
                method='average', 
                metric='euclidean', 
@@ -104,40 +99,9 @@ sns.clustermap(df,
                cbar_pos=(0.02, 0.8, 0.05, 0.18), 
                tree_kws=None, 
                cmap=cc.cm.CET_CBL1)
-plt.show()
-'''
+''')
 
-@click.group()
-def cli():
-    """
-    CLI tool to extend Jupyter notebooks for data visualization.
-    """
-    pass
-
-@click.command()
-@click.argument('filename')
-@click.argument('datafile')
-def heatmap(filename, datafile):
-    """
-    Add cells to a notebook for loading data and displaying a heatmap.
-    """
-    notebook = load_or_create_notebook(filename)
-    add_cell(notebook, 'code', create_data_loading_cell(datafile))
-    add_cell(notebook, 'code', create_heatmap_cell())
-    save_notebook(notebook, filename)
-
-@click.command()
-@click.argument('filename')
-@click.argument('datafile')
-def clustermap(filename, datafile):
-    """
-    Add cells to a notebook for loading data and displaying a clustermap.
-    """
-    notebook = load_or_create_notebook(filename)
-    add_cell(notebook, 'code', create_data_loading_cell(datafile))
-    add_cell(notebook, 'code', create_clustermap_cell())
-    save_notebook(notebook, filename)
-
+cli.add_command(load_dataframe)
 cli.add_command(heatmap)
 cli.add_command(clustermap)
 
